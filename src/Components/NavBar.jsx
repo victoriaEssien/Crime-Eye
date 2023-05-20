2fb4ff4b3ac3cb12e526e8af0735a33568927c2d
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-05-20 11:32:29 +0100

    styled the navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,34 @@
+
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <div>
+        <Navbar expand="lg" className="nav-bar">
+            <Container>
+            <Navbar.Brand href="#home" className='logo'>Crime Eye</Navbar.Brand>
+            <Navbar.Toggle className='menu' aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto links">
+            <Nav.Link href="#home" className='nav-links'>Home</Nav.Link>
+            <Nav.Link href="#link" className='nav-links'>About</Nav.Link>
+            <Nav.Link href="#link" className='nav-links'>User guide</Nav.Link>
+            <Nav.Link href="#link" className='nav-links'>Features</Nav.Link>
+            </Nav>
+            <Nav className='ms-auto links'>
+                <Link to="/register">
+                    <Button>Get Started</Button>
+                </Link>
+            </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default NavBar
--- src/Sections/HeroSection.jsx
+++ src/Sections/HeroSection.jsx
@@ -2,12 +2,18 @@
 import { Link } from "react-router-dom"
 import Button from 'react-bootstrap/Button';
 
+// Components
+
+import NavBar from "../Components/NavBar";
+
 
 function HeroSection() {
   return (
-    <div>
-        <h1>Welcome to Crime Eye</h1>
-        <div>
+    <div className="hero-section">
+      <NavBar />
+        <h1>Equipping Users to Outsmart Scammers</h1>
+        <p>Crime Eye helps users to stay one step ahead of scammers by providing an easy-to-use platform for learning about their latest tricks and reporting to the authorities.</p>
+        <div className="hero-cta">
         <Link to="/register">
             <Button>Get Started</Button>
         </Link>
