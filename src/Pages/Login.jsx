da139aad595b5939939d6ac55f045034a340607e
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-05-20 12:41:18 +0100

    styled auth forms

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,26 +1,42 @@
 
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 
+import InputGroup from 'react-bootstrap/InputGroup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+
 function Login() {
+
+    const [passwordVisible, setPasswordVisible] = useState(false)
+
+
+    function handlePasswordVisibility() {
+        setPasswordVisible(!passwordVisible)
+    }
+
   return (
-    <div>
+    <div className='form-container'>
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" size='lg' placeholder="Enter email" />
+                <Form.Control className='input' type="email" size='lg' placeholder="Enter email" required/>
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" aria-describedby='passwordHelpBlock' size='lg' placeholder="Enter your password" />
-            </Form.Group>
+            <Form.Label htmlFor="pass">Password</Form.Label>
+            <InputGroup className="mb-3" size="lg">
+            <Form.Control id="pass" className="pass-input" type={passwordVisible ? "text" : "password"} name="password" size="lg" required/>
+            <InputGroup.Text>
+              <FontAwesomeIcon className="password-icon" onClick={handlePasswordVisibility} icon={passwordVisible ? faEyeSlash : faEye} />
+            </InputGroup.Text>
+            </InputGroup>
 
             <div className='d-grid'>
-                <Button type='submit'>Login</Button>
+                <Button type='submit' className='auth-btn'>Login</Button>
             </div>
-            <p>Don&apos;t have an account? <Link to="/register">Register</Link></p>
+            <p className='helper-text'>Don&apos;t have an account? <Link to="/register" className='helper-link'>Register</Link></p>
         </Form>
     </div>
   )
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,35 +1,53 @@
 
+import { useState } from "react"
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
 
+import InputGroup from 'react-bootstrap/InputGroup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+
 function SignUp() {
+
+    // const [formError, setFormError] = useState(null)
+
+    const [passwordVisible, setPasswordVisible] = useState(false)
+
+
+    function handlePasswordVisibility() {
+        setPasswordVisible(!passwordVisible)
+    }
+
   return (
-    <div>
+    <div className="form-container">
         <Form>
             <Form.Group className="mb-3" controlId="formBasicName">
                 <Form.Label>Full Name</Form.Label>
-                <Form.Control type="text" size='lg' placeholder="Enter your full name" />
+                <Form.Control className="input" type="text" size='lg' placeholder="Enter your full name" required/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" size='lg' placeholder="Enter email" />
+                <Form.Control className="input" type="email" size='lg' placeholder="Enter email" required/>
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" aria-describedby='passwordHelpBlock' size='lg' placeholder="Enter your password" />
-            </Form.Group>
+            <Form.Label htmlFor="pass">Password</Form.Label>
+            <InputGroup className="mb-3" size="lg">
+            <Form.Control id="pass" className="pass-input" type={passwordVisible ? "text" : "password"} name="password" size="lg" required/>
+            <InputGroup.Text>
+              <FontAwesomeIcon className="password-icon" onClick={handlePasswordVisibility} icon={passwordVisible ? faEyeSlash : faEye} />
+            </InputGroup.Text>
+            </InputGroup>
 
-            <Form.Text id="passwordHelpBlock" muted>
+            <Form.Text id="passwordHelpBlock" className="form-text" muted>
             Your password must be 8-16 characters long, contain letters and numbers, and must not contain spaces or emoji.
             </Form.Text>
 
             <div className='d-grid'>
-                <Button type='submit'>Create Account</Button>
+                <Button type='submit' className="auth-btn">Create Account</Button>
             </div>
-            <p>Already have an account? <Link to="/login">Log in</Link></p>
+            <p className="helper-text">Already have an account? <Link className="helper-link" to="/login">Log in</Link></p>
         </Form>
     </div>
   )
